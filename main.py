fc5a0c72d4c6f838ad94396e7919a1c8884f78ce
alliedbrother <saiakhil121@gmail.com>
2025-04-23 18:22:48 -0400

    first_push

---
+++ main.py
@@ -0,0 +1,16 @@
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [
+#     "requests",
+#     "rich",
+# ]
+# ///
+
+import sys
+from rich import print 
+import requests
+
+
+print(sys.version)
+
+print("Hello, World!")
